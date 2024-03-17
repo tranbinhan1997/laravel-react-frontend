14d44918b7af6c7f91588bdf66d383ae019b7af9
AnTran <tranbinhan1997@gmail.com>
2024-03-17 18:54:35 +0700

    frontend

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -74,7 +74,7 @@ export default function DefaultLayout() {
 								clipRule="evenodd"></path>
 						</svg>
 					</button>
-					<div className="hidden w-full md:block md:w-auto" id="navbar-default">
+					<div className="w-full md:block md:w-auto" id="navbar-default">
 						<ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
 								<NavLink
